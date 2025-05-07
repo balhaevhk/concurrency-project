08ce57cf0eda7a90be7c6e2177c7955dd67fa660
Hasan <balhaevhk@icloud.com>
2025-05-07 19:55:39 +0300

    Implemented the core architecture of the project

--- cmd/concurrency/main.go
+++ cmd/concurrency/main.go
@@ -1,34 +1,28 @@
 package main
 
 import (
-	"errors"
+	"concurrency/internal/user"
 	"fmt"
 )
 
-type User struct {
-	ID string
-	Name string
-	Balance float64
-}
 
-func (u *User) Deposit(amount float64) {
-	u.Balance += amount 
-}
-
-func (u *User) Withdraw(amount float64) error {
-	if u.Balance < amount {
-		return errors.New("insufficient funds on balance")
-	}
-	u.Balance -= amount
-	return nil
-}
 
 func main() {
-	user1 := &User{ID: "1", Name: "John", Balance: 1000}
-	user2 := &User{ID: "1", Name: "Petr", Balance: 3000}
+	user1 := &user.User{ID: "1", Name: "John", Balance: 1000}
+	user2 := &user.User{ID: "1", Name: "Petr", Balance: 3000}
 
 	user1.Deposit(4000)
+	user1.Withdraw(500)
 	fmt.Println(user1)
+
 	user2.Deposit(4000)
+	err := user2.Withdraw(10000)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	err = user2.Withdraw(5000)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
 	fmt.Println(user2)
 }
